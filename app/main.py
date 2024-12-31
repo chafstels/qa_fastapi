08a906194323caec55aedba6e8db348113a40e24
Anton <antonchzhu@gmail.com>
2024-12-31 13:00:59 +0600

    add requirements

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.routers import users, logs, auth
+import uvicorn
 
 app = FastAPI()
 
@@ -11,3 +12,7 @@ app.include_router(logs.router)
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the FastAPI backend for project management"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
